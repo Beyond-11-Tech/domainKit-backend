d81330dbe71748e978f2b010ffa66b7f08f133ab
mb-b11 <myles.hunt@beyond11.tech>
2025-04-23 22:33:45 +0100

    update endpoint to use bindings for param validation

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"net/http"
-	"strings"
 
+	"github.com/fvbock/endless"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,36 +18,30 @@ func main() {
 	router := gin.Default()
 	router.GET("/domain", getRecordForDomain)
 
-	router.Run(":8080")
+	endless.ListenAndServe(":8080", router)
 }
 
 func getRecordForDomain(c *gin.Context) {
-	domain, present := c.GetQuery("domain")
+	type queryParams struct {
+		Address string `form:"address" binding:"required"`
+	}
 	returnCode := http.StatusOK
-
 	var results []domainResult
 
-	if !present {
-		c.String(http.StatusBadRequest, "please include a 'domain' query param")
+	var params queryParams
+
+	err := c.ShouldBindQuery(&params)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	for _, registrar := range domainList {
-		buf, err := executeDigQuery(registrar, domain)
-		record := strings.Fields(buf.String())
-
-		if err != nil {
-			results = append(results, domainResult{
-				Registrar: "error",
-				Record:    []string{"Domain: " + registrar, err.Error()},
-			})
-			returnCode = http.StatusInternalServerError
-			continue
-		}
+		buf := ExecuteARecordQuery(registrar, params.Address)
 
 		results = append(results, domainResult{
 			Registrar: registrar,
-			Record:    record,
+			Record:    buf,
 		})
 	}
 	c.JSON(returnCode, results)
